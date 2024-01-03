014f556df11642bb454c98200036182da8d2c9da
Phan Nguyễn Gia Lâm <lampng_1102@hotmail.com>
2024-01-03 23:02:34 +0700

    update stastitic

--- routes/statisticAPI.js
+++ routes/statisticAPI.js
@@ -24,29 +24,29 @@ router.get('/', (req, res) => {
 });
 router.get('/totalRevenue', async (req, res) => {
     try {
-        let query = { status: 'Đã thanh toán' };
+        // let query = { status: 'Đã thanh toán' };
+        
+        // // Thêm điều kiện tìm kiếm theo tháng năm nếu được cung cấp
+        // if (req.query.month && req.query.year) {
+        //     const startOfMonth = new Date(`${req.query.year}-${req.query.month}-01`);
+        //     const endOfMonth = new Date(`${req.query.year}-${req.query.month + 1}-01`);
+        //     query = {
+        //         ...query,
+        //         signingDate: { $gte: startOfMonth, $lt: endOfMonth },
+        //     };
+        // }
+        // query = {
+        //     ...query,
+        //     signingDate: { $exists: true, $ne: null },
+        // };
 
-        // Thêm điều kiện tìm kiếm theo tháng năm nếu được cung cấp
-        if (req.query.month && req.query.year) {
-            const startOfMonth = new Date(`${req.query.year}-${req.query.month}-01`);
-            const endOfMonth = new Date(`${req.query.year}-${req.query.month + 1}-01`);
-            query = {
-                ...query,
-                signingDate: { $gte: startOfMonth, $lt: endOfMonth },
-            };
-        }
-        query = {
-            ...query,
-            signingDate: { $exists: true, $ne: null },
-        };
+        // // Truy vấn các hợp đồng theo điều kiện
+        // const contracts = await contractModels.find(query);
 
-        // Truy vấn các hợp đồng theo điều kiện
-        const contracts = await contractModels.find(query);
-
-        // Tính tổng số tiền từ các hợp đồng
-        const totalAmount = contracts.reduce((acc, contract) => acc + contract.priceTotal, 0);
-
-        res.json({ totalAmount });
+        // // Tính tổng số tiền từ các hợp đồng
+        // const totalAmount = contracts.reduce((acc, contract) => acc + contract.priceTotal, 0);
+        const revenue = await calculateRevenue(req.query.month, req.query.year);
+        res.json({ revenue });
     } catch (error) {
         console.error('Lỗi khi lấy tổng số tiền:', error);
         res.status(500).json({ error: 'Đã xảy ra lỗi' });
@@ -132,4 +132,42 @@ router.get('/wedding-outfit-usage', async (req, res) => {
         res.status(500).json({ error: 'Internal Server Error' });
     }
 });
+
+async function calculateRevenue(month, year) {
+    let matchCriteria = {
+        status: 'Đã thanh toán',
+    };
+
+    if (month && year) {
+        const startDate = new Date(year, month - 1, 1);
+        const endDate = new Date(year, month, 0, 23, 59, 59, 999);
+
+        matchCriteria.workDate = {
+            $gte: startDate,
+            $lte: endDate,
+        };
+    } else if (year) {
+        const startDate = new Date(year, 0, 1);
+        const endDate = new Date(year, 11, 31, 23, 59, 59, 999);
+
+        matchCriteria.workDate = {
+            $gte: startDate,
+            $lte: endDate,
+        };
+    }
+
+    const result = await contractModels.aggregate([
+        {
+            $match: matchCriteria,
+        },
+        {
+            $group: {
+                _id: null,
+                totalRevenue: { $sum: '$priceTotal' },
+            },
+        },
+    ]);
+
+    return result.length > 0 ? result[0].totalRevenue : 0;
+}
 module.exports = router;
